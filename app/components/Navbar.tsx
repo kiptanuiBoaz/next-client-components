7c2d21f7472294da2695a638314f449e2908f286
Kiptanui Boaz <boaserem022@gmail.com>
2023-12-31 06:10:46 +0300

    add new proj

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-// import Search from "./Search"
+import Search from "./Search"
 
 export default function Navbar() {
     return (
@@ -7,7 +7,7 @@ export default function Navbar() {
             <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
                 <Link href="/">WikiRocket!</Link>
             </h1>
-            {/* <Search /> */}
+            <Search />
         </nav>
     )
 }
